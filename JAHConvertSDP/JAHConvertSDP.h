7ae692321da2b0f161558b12b3e2e5040681e006
Jon Hjelle <hjon@andyet.net>
2014-05-16 13:26:59 -0700

    Add JAHConvertSDP files

---
+++ JAHConvertSDP/JAHConvertSDP.h
@@ -0,0 +1,13 @@
+//
+//  JAHConvertSDP.h
+//  JAHConvertSDP
+//
+//  Created by Jon Hjelle on 5/16/14.
+//  Copyright (c) 2014 Jon Hjelle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JAHConvertSDP : NSObject
+
+@end
---
+++ JAHConvertSDP/JAHConvertSDP.m
@@ -0,0 +1,13 @@
+//
+//  JAHConvertSDP.m
+//  JAHConvertSDP
+//
+//  Created by Jon Hjelle on 5/16/14.
+//  Copyright (c) 2014 Jon Hjelle. All rights reserved.
+//
+
+#import "JAHConvertSDP.h"
+
+@implementation JAHConvertSDP
+
+@end
